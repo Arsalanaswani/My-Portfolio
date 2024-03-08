c3865fb86bc5fd84d8010efbba8a642477014fb2
Arsalanaswani <117674124+Arsalanaswani@users.noreply.github.com>
2024-03-09 00:15:01 +0500

    Update python.py

--- Portfolio/Helloworld/python.py
+++ Portfolio/Helloworld/python.py
@@ -1,13 +1,1 @@
-from twilio.rest import Client
 
-account_sid = 'your_account_sid'
-auth_token = 'your_auth_token'
-client = Client(account_sid, auth_token)
-
-for i in range(20):
-    message = client.messages.create(
-        body='Hello, this is message number ' + str(i+1),
-        from_='whatsapp:+14155238886',
-        to='whatsapp:+1234567890'
-    )
-    print('Message sent: ' + message.sid)
